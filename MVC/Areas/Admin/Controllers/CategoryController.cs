0c462a92592c547c5324cf88c2549f7647319d58
erhanzorlu <zorlu.com@gmail.com>
2023-04-04 23:39:19 +0300

    .

--- MVC/Areas/Admin/Controllers/CategoryController.cs
+++ MVC/Areas/Admin/Controllers/CategoryController.cs
@@ -1,9 +1,13 @@
-﻿using MVC.Areas.Admin.Data.AdminPageVMs;
+﻿using Microsoft.Ajax.Utilities;
+using MVC.Areas.Admin.Data.AdminPageVMs;
 using Project.BLL.Generic_Repository.ConcRep;
+using Project.ENTITIES.Models;
 using Project.ViewModels.VMClasses;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Web.Mvc;
+using WebGrease.Css.Ast;
 
 namespace MVC.Areas.Admin.Controllers
 {
@@ -56,6 +60,47 @@ namespace MVC.Areas.Admin.Controllers
 
                 };            
             return View(alistVm);
+        }
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCategory(AdminCategoryVM categoryVM)
+        {
+            Category category = new Category()
+            {
+                CategoryName = categoryVM.CategoryName,
+                Description = categoryVM.Description,
+
+            };
+            _cRep.Add(category);
+            return RedirectToAction("ListCategories");
+        }
+        public ActionResult UpdateCategory(int id)
+        {
+            Category cat = _cRep.Find(id);
+            AdminCategoryVM categoryVM = new AdminCategoryVM()
+            {
+                ID = cat.ID,
+                CategoryName = cat.CategoryName,
+                Description = cat.Description
+            };        
+
+            return View(categoryVM);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateCategory(AdminCategoryVM categoryVM)
+        {
+            Category secilen = _cRep.Find(categoryVM.ID);
+            secilen.ID = categoryVM.ID;
+            secilen.CategoryName = categoryVM.CategoryName;
+            secilen.Description = categoryVM.Description;
+            _cRep.Update(secilen);
+            
+            return RedirectToAction("ListCategories");
         }
     }
 }
